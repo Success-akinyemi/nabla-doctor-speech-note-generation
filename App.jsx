dfe3ed978cac28d75d0bee9cec5a4122d212ea6c
Success-akinyemi <brownsamuel915@gmail.com>
2024-01-13 04:18:15 -0800

    New

--- App.jsx
+++ App.jsx
@@ -54,7 +54,7 @@ const App = () => {
   }
     useEffect(() => {
 
-        initializeWsCnx();
+        
         
     }, [])
     
@@ -79,64 +79,76 @@ const App = () => {
 
     
         if (navigator.mediaDevices && navigator.mediaDevices.getUserMedia) {
-            // Ask authorization to access the microphone
-            const stream = await navigator.mediaDevices.getUserMedia({
-                audio: {
-                    deviceId: "default",
-                    sampleRate: 16000,
-                    sampleSize: 16,
-                    channelCount: 1,
-                },
-                video: false,
-            });
-            audioContext = new AudioContext({ sampleRate: 16000 });
-            await audioContext.audioWorklet.addModule("rawPcm16Processor.js")
-            pcmWorker = new AudioWorkletNode(audioContext, rawPCM16WorkerName, {
-                outputChannelCount: [1],
-            });
-            mediaSource = audioContext.createMediaStreamSource(stream);
-            mediaSource.connect(pcmWorker);
-    
-            // pcm post on message
-            pcmWorker.port.onmessage = (msg) => {
-                const pcm16iSamples = msg.data;
-                const audioAsBase64String = btoa(
-                    String.fromCodePoint(...new Uint8Array(pcm16iSamples.buffer)),
-                );
-                if (websocket.readyState !== websocket.OPEN) {
-                    console.error("Websocket is no longer open")
-                    return;
+           try {
+               // Ask authorization to access the microphone
+               const stream = await navigator.mediaDevices.getUserMedia({
+                   audio: {
+                       deviceId: "default",
+                       sampleRate: 16000,
+                       sampleSize: 16,
+                       channelCount: 1,
+                   },
+                   video: false,
+               });
+               audioContext = new AudioContext({ sampleRate: 16000 });
+               await audioContext.audioWorklet.addModule("rawPcm16Processor.js")
+               pcmWorker = new AudioWorkletNode(audioContext, rawPCM16WorkerName, {
+                   outputChannelCount: [1],
+               });
+               mediaSource = audioContext.createMediaStreamSource(stream);
+               mediaSource.connect(pcmWorker);
+       
+               // pcm post on message
+               pcmWorker.port.onmessage = (msg) => {
+                   const pcm16iSamples = msg.data;
+                   const audioAsBase64String = btoa(
+                       String.fromCodePoint(...new Uint8Array(pcm16iSamples.buffer)),
+                   );
+                   if (websocket.readyState !== websocket.OPEN) {
+                       console.error("Websocket is no longer open")
+                       return;
+                   }
+                   // Send the audio chunk to the websocket cnx
+                   websocket.send(
+                       JSON.stringify({
+                           object: "audio_chunk",
+                           payload: audioAsBase64String,
+                           stream_id: "stream1",
+                       })
+                   );
+               }
+       
+               const config = {
+                   object: "listen_config",
+                   output_objects: ["transcript_item", "note"],
+                   encoding: "pcm_s16le",
+                   sample_rate: 16000,
+                   language: "en",
+                   streams: [
+                       { id: "stream1", speaker_type: "doctor" },
+                   ],
+               };
+               websocket.send(JSON.stringify(config));
+       
+               // pcm start
+               pcmWorker.port.start();
+            
+           } catch (error) {
+                console.log('ERROR ACCESSING MIC:', error)
+                if (error.name === 'NotAllowedError') {
+                    // Handle user denial of microphone access
+                    console.log('User denied microphone access');
                 }
-                // Send the audio chunk to the websocket cnx
-                websocket.send(
-                    JSON.stringify({
-                        object: "audio_chunk",
-                        payload: audioAsBase64String,
-                        stream_id: "stream1",
-                    })
-                );
-            }
-    
-            const config = {
-                object: "listen_config",
-                output_objects: ["transcript_item", "note"],
-                encoding: "pcm_s16le",
-                sample_rate: 16000,
-                language: "en",
-                streams: [
-                    { id: "stream1", speaker_type: "doctor" },
-                ],
-            };
-            websocket.send(JSON.stringify(config));
-    
-            // pcm start
-            pcmWorker.port.start();
+            document.getElementById("start").removeAttribute('disabled')
+           }
+           
         } else {
             console.error("Microphone audio stream is not accessible on this browser");
         }
     }
     
     const startRecording = () => {
+        initializeWsCnx();
         startRecordingAsync()
             .then()
             .catch((err) => {
